27527a3a93c79d3dd65abb9545b5c72191dbbe90
Gonçalo Rosa <goncalojmrosa@gmail.com>
2024-03-24 14:15:46 +0000

    Add profile tabs

--- frontend/src/pages/perfil.tsx
+++ frontend/src/pages/perfil.tsx
@@ -1,6 +1,7 @@
 import { FamilyMemberCard } from "@/components/family-member-card";
 import { NavBar } from "@/components/navbar";
 import { NewfamilyMember } from "@/components/new-family-member";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export function Perfil() {
   const familiares = [
@@ -27,12 +28,28 @@ export function Perfil() {
   return (
     <div>
       <NavBar />
-      <div className="container mx-auto mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 auto-rows-[250px]">
-        <NewfamilyMember />
+      <div className="container mx-auto mt-5">
+        <Tabs defaultValue="account" className="w-full">
+          <TabsList className="w-full bg-slate-200">
+            <TabsTrigger className="w-1/2 font-bold" value="account">
+              Conta
+            </TabsTrigger>
+            <TabsTrigger className="w-1/2 font-bold" value="family">
+              Familiares
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="account">Mostrar dados.</TabsContent>
+          <TabsContent
+            value="family"
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 auto-rows-[250px]"
+          >
+            <NewfamilyMember />
 
-        {familiares.map((familiar) => {
-          return <FamilyMemberCard key={familiar.ID} familiar={familiar} />;
-        })}
+            {familiares.map((familiar) => {
+              return <FamilyMemberCard key={familiar.ID} familiar={familiar} />;
+            })}
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   );
