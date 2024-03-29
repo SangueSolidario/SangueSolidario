23ce7a74f665b79c7964dc8f1c7522214a166fdc
Gonçalo Rosa <goncalojmrosa@gmail.com>
2024-03-29 17:31:40 +0000

    Disable cors

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    "https://cors-anywhere.herokuapp.com/https://sanguesolidario.azure-api.net/api/",
+  baseURL: "https://sanguesolidario.azure-api.net/api/",
 });
 
 export default api;
